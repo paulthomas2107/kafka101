6b0060ab19d6e84c09195f256b6d0d7540f8e5bf
Paul Thomas <paulthomas2107@gmail.com>
2021-06-02 19:03:41 +0100

    Initial

---
+++ topic.js
@@ -0,0 +1,11 @@
+const { Kafka } = require("kafkajs");
+
+async function run() {
+    try {
+        const kafka = new Kafka({
+          
+      })
+  } catch (ex) {
+    console.error(`Something happeed  ${ex}`);
+  }
+}
